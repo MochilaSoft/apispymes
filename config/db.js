fcdc33bbcc27eb54ca496d800c2d6d5cfbf539e8
MochilaSoft <mochilasoft@gmail.com>
2025-05-24 13:41:19 -0300

    agregando puerto bd

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ||  22363
 });
 
 module.exports = db;
